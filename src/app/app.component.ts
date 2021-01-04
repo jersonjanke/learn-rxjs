ac218f222d58ed859dc3bf1d338e92400c5e9953
jerson janke <jersonjanke@gmail.com>
2021-01-04 18:05:58 -0300

    create first observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'learn-rxjs';
+export class AppComponent implements OnInit {
+  
+  observer = {
+    next: value => console.log('next', value),
+    error: error => console.log('error', error),
+    complete: () => console.log('complete')
+  }
+
+  constructor() {}
+ 
+  ngOnInit(): void {
+    const observable = new Observable(subscriber => {
+      subscriber.next('Hello');
+      subscriber.next('World');
+      subscriber.complete()
+    })
+
+    observable.subscribe(this.observer)
+  }
+
 }
